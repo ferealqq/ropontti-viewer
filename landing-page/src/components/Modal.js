24d40538edc963362dedcac90a9d10e3c76467d0
ferealqq <pekka.mattinen@webso.fi>
2022-05-14 12:19:22 +0300

    preloading

--- landing-page/src/components/Modal.js
+++ landing-page/src/components/Modal.js
@@ -1,40 +1,43 @@
-import React, { useState, useEffect, useContext,createContext} from "react";
-import {roponttiContext} from '../App';
+import React, { useState, useEffect, useContext, createContext } from "react";
+import { roponttiContext } from "../App";
 import Stream from "../Stream";
 
-
 const Modal = (props) => {
-    const { order, destination, name, song, setStream, time} = useContext(roponttiContext);
+  const { order, destination, name, song, setStream, time } =
+    useContext(roponttiContext);
 
-    const handleClick = () => {
-        setStream(true);
-    }
-    // console.log(order);
-    // console.log(destination);
-    // console.log(name);
-    // console.log(song);
-    if(order === null || destination === null || name === null || song === null){
-        return(
-            <div>
-                <h1>Loading...</h1>
-            </div>
-        )
-    }
+  const handleClick = () => {
+    setStream(true);
+  };
+  // console.log(order);
+  // console.log(destination);
+  // console.log(name);
+  // console.log(song);
+  if (
+    order === null ||
+    destination === null ||
+    name === null ||
+    song === null
+  ) {
     return (
-        <div>
-            Nimi: {name} <br />
-            Osoite: {destination}  <br />
-            Biisi: {song} <br />
-            Tilaus: {order.map((item) => {
-                return (
-                    <div key={item}>
-                        {item}
-                    </div>
-                )
-            })}
-            <Stream startTime={time}/>
-        </div>
-    )
-}
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      Nimi: {name} <br />
+      Osoite: {destination} <br />
+      Biisi: {song} <br />
+      Tilaus:{" "}
+      {order.map((item) => {
+        return <div key={item}>{item}</div>;
+      })}
+      {/* <Stream startTime={time}/> */}
+      <Stream startTime={130} />
+    </div>
+  );
+};
 
 export default Modal;
